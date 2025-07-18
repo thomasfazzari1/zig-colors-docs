d177896c8fe7d422cd15dd964ba5342a5a326287
thomasfazzari1 <fazzarithomas5@gmail.com>
2025-07-18 21:09:36 +0200

    Deploy documentation

---
+++ assets/js/5e95c892.14c1f8dd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkzig_colors_docs=self.webpackChunkzig_colors_docs||[]).push([[647],{7121:(s,e,r)=>{r.r(e),r.d(e,{default:()=>n});r(6540);var c=r(4164),o=r(7559),u=r(5500),a=r(2831),l=r(1656),d=r(4848);function n(s){return(0,d.jsx)(u.e3,{className:(0,c.A)(o.G.wrapper.docsPages),children:(0,d.jsx)(l.A,{children:(0,a.v)(s.route.routes)})})}}}]);
